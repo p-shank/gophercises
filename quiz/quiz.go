5480c69b51a0226315fd6c79a83618f99e866346
p-shank <89237061+p-shank@users.noreply.github.com>
2024-10-14 21:57:55 +0530

    adding changes for quiz exercise

---
+++ quiz/quiz.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Questions struct {
+	question string
+	answer   string
+}
+
+func main() {
+	fmt.Println("Start Of Quiz...")
+}
+
+func readCSV(string fileName) Questions {
+
+}
